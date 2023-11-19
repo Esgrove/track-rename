bc7029d97a37257e5b48e89b2d05602906b51923
Esgrove <esgrove@outlook.com>
2023-11-19 21:34:34 +0200

    add shell script

---
+++ renamer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -eo pipefail
+
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Check platform:
+case "$(uname -s)" in
+    "Darwin")
+        export PLATFORM="mac"
+        ;;
+    "MINGW"*)
+        export PLATFORM="windows"
+        ;;
+    *)
+        export PLATFORM="linux"
+        ;;
+esac
+
+if [ "$PLATFORM" = windows ]; then
+    music_path="/d/Dropbox/DJ MUSIC"
+    python="python"
+else
+    music_path="$HOME/Dropbox/DJ MUSIC"
+    python="python3"
+fi
+
+$python "$DIR/renamer.py" "$music_path"
