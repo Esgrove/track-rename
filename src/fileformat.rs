7c0fbf648a12f82cd37b5f90d3cc4949015d5a51
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-01-29 21:53:16 +0200

    add first test cases

--- src/fileformat.rs
+++ src/fileformat.rs
@@ -3,7 +3,7 @@ use std::fmt::Display;
 
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum FileFormat {
     Mp3,
     Aif,
@@ -28,5 +28,34 @@ impl Display for FileFormat {
             FileFormat::Aif => "aif".to_string(),
         };
         write!(f, "{}", str)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_from_str_valid_formats() {
+        assert_eq!(FileFormat::from_str("mp3").unwrap(), FileFormat::Mp3);
+        assert_eq!(FileFormat::from_str("Mp3").unwrap(), FileFormat::Mp3);
+        assert_eq!(FileFormat::from_str("MP3").unwrap(), FileFormat::Mp3);
+        assert_eq!(FileFormat::from_str("aif").unwrap(), FileFormat::Aif);
+        assert_eq!(FileFormat::from_str("aiff").unwrap(), FileFormat::Aif);
+        assert_eq!(FileFormat::from_str("Aif").unwrap(), FileFormat::Aif);
+        assert_eq!(FileFormat::from_str("Aiff").unwrap(), FileFormat::Aif);
+        assert_eq!(FileFormat::from_str("AIF").unwrap(), FileFormat::Aif);
+        assert_eq!(FileFormat::from_str("AIFF").unwrap(), FileFormat::Aif);
+    }
+
+    #[test]
+    fn test_from_str_invalid_format() {
+        assert!(FileFormat::from_str("wav").is_err());
+    }
+
+    #[test]
+    fn test_display() {
+        assert_eq!(format!("{}", FileFormat::Mp3), "mp3");
+        assert_eq!(format!("{}", FileFormat::Aif), "aif");
     }
 }
