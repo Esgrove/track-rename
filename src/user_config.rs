2cdf3e6059b6d62e4f695a8a3793558bb94804f3
Esgrove <esgrove@outlook.com>
2024-03-16 13:04:21 +0200

    implement display for user config

--- src/user_config.rs
+++ src/user_config.rs
@@ -1,6 +1,7 @@
-use std::{fs, path::PathBuf};
+use std::{fmt, fs, path::PathBuf};
 
 use anyhow::{anyhow, Context};
+use colored::Colorize;
 use dirs::home_dir;
 use serde::Deserialize;
 
@@ -36,5 +37,32 @@ fn user_config_file_path() -> anyhow::Result<PathBuf> {
     match config_path.exists() {
         true => Ok(config_path),
         false => Err(anyhow!("Config file not found: {}", config_path.display())),
+    }
+}
+
+impl fmt::Display for FileExclusionList {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let files_formatted: String = self
+            .files
+            .iter()
+            .map(|file| format!("    {}", file))
+            .collect::<Vec<String>>()
+            .join("\n");
+        write!(f, "Excluded Files:\n{}", files_formatted)
+    }
+}
+
+impl fmt::Display for UserConfig {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Convert failed: {}",
+            if self.convert_failed {
+                "true".green()
+            } else {
+                "false".yellow()
+            }
+        )?;
+        write!(f, "{}", self.exclude)
     }
 }
