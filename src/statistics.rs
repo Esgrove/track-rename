d8228fe40c962f137beaf499fab192540c7a84e1
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-04-17 14:26:29 +0300

    don't print stats if nothing needs to change

--- src/statistics.rs
+++ src/statistics.rs
@@ -16,22 +16,37 @@ pub struct Statistics {
     pub num_converted: usize,
 }
 
+impl Statistics {
+    pub fn no_changes(&self) -> bool {
+        self.num_tags == 0
+            && self.num_to_rename == 0
+            && self.num_to_remove == 0
+            && self.num_duplicates == 0
+            && self.num_failed == 0
+            && self.num_converted == 0
+    }
+}
+
 impl fmt::Display for Statistics {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "{}", "Statistics:".bold())?;
-        writeln!(f, "Fix tags:   {} / {}", self.num_tags_fixed, self.num_tags)?;
-        writeln!(f, "Renamed:    {} / {}", self.num_renamed, self.num_to_rename)?;
-        if self.num_converted > 0 {
-            writeln!(f, "Converted:  {}", self.num_converted)?;
-        }
-        if self.num_to_remove > 0 {
-            writeln!(f, "Deleted:    {} / {}", self.num_removed, self.num_to_remove)?;
-        }
-        if self.num_duplicates > 0 {
-            writeln!(f, "Duplicate:  {}", self.num_duplicates)?;
-        }
-        if self.num_failed > 0 {
-            writeln!(f, "Failed:     {}", self.num_failed)?;
+        if self.no_changes() {
+            println!("{}", "All Good".green());
+        } else {
+            writeln!(f, "{}", "Updated:".bold())?;
+            writeln!(f, "Fix tags:   {} / {}", self.num_tags_fixed, self.num_tags)?;
+            writeln!(f, "Renamed:    {} / {}", self.num_renamed, self.num_to_rename)?;
+            if self.num_converted > 0 {
+                writeln!(f, "Converted:  {}", self.num_converted)?;
+            }
+            if self.num_to_remove > 0 {
+                writeln!(f, "Deleted:    {} / {}", self.num_removed, self.num_to_remove)?;
+            }
+            if self.num_duplicates > 0 {
+                writeln!(f, "Duplicate:  {}", self.num_duplicates)?;
+            }
+            if self.num_failed > 0 {
+                writeln!(f, "Failed:     {}", self.num_failed)?;
+            }
         }
         Ok(())
     }
