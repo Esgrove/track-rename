77a49193d9a3a2d8f5696ad72828e3c7fdb91b1b
Esgrove <esgrove@outlook.com>
2024-05-18 16:22:19 +0300

    reorder util functions

--- src/utils.rs
+++ src/utils.rs
@@ -12,57 +12,12 @@ use unicode_normalization::UnicodeNormalization;
 
 use crate::track::Track;
 
-/// Ask user to confirm action.
-/// Note: everything except `n` is a yes.
-pub fn confirm() -> bool {
-    print!("Proceed (y/n)? ");
-    io::stdout().flush().expect("Failed to flush stdout");
-    let mut ans = String::new();
-    io::stdin().read_line(&mut ans).expect("Failed to read line");
-    ans.trim().to_lowercase() != "n"
-}
-
-/// Convert the given path to be relative to the current working directory.
-/// Returns the original path if the relative path cannot be created.
-pub fn get_relative_path_from_current_working_directory(path: &Path) -> PathBuf {
-    env::current_dir()
-        .map(|current_dir| path.strip_prefix(&current_dir).unwrap_or(path).to_path_buf())
-        .unwrap_or(path.to_path_buf())
-}
-
-/// Convert path to string with invalid unicode handling.
-pub fn path_to_string(path: &Path) -> String {
-    if let Some(string) = path.to_str() {
-        string.to_string()
+/// Format bool value as a coloured string
+pub fn colorize_bool(value: bool) -> ColoredString {
+    if value {
+        "true".green()
     } else {
-        let string = path.to_string_lossy().to_string().replace('\u{FFFD}', "");
-        eprintln!("{}", "Path contains invalid unicode".red());
-        eprintln!("{:?}", path);
-        eprintln!("{}", string);
-        string
-    }
-}
-
-/// Get relative path and convert to string with invalid unicode handling.
-pub fn path_to_string_relative(path: &Path) -> String {
-    path_to_string(&get_relative_path_from_current_working_directory(path))
-}
-
-/// Try to read tags from file.
-/// Will return empty tags when there are no tags.
-pub fn read_tags(track: &Track) -> Option<Tag> {
-    match Tag::read_from_path(&track.path) {
-        Ok(tag) => Some(tag),
-        Err(Error {
-            kind: ErrorKind::NoTag, ..
-        }) => {
-            println!("\n{}", format!("No tags: {}", track).yellow());
-            Some(Tag::new())
-        }
-        Err(error) => {
-            eprintln!("\n{}", format!("Failed to read tags for: {}\n{}", track, error).red());
-            None
-        }
+        "false".yellow()
     }
 }
 
@@ -97,6 +52,103 @@ pub fn color_diff(old: &str, new: &str) -> (String, String) {
     (old_diff, new_diff)
 }
 
+/// Ask user to confirm action.
+/// Note: everything except `n` is a yes.
+pub fn confirm() -> bool {
+    print!("Proceed (y/n)? ");
+    io::stdout().flush().expect("Failed to flush stdout");
+    let mut ans = String::new();
+    io::stdin().read_line(&mut ans).expect("Failed to read line");
+    ans.trim().to_lowercase() != "n"
+}
+
+/// Check if the given path contains the sub path.
+pub fn contains_subpath(main_path: &Path, subpath: &Path) -> bool {
+    let main_components: Vec<_> = main_path.components().collect();
+    let sub_components: Vec<_> = subpath.components().collect();
+
+    if sub_components.len() > main_components.len() {
+        return false;
+    }
+
+    // Find the start index of the first subpath component in the main path
+    if let Some(first_sub_component) = sub_components.first() {
+        for (index, main_component) in main_components.iter().enumerate() {
+            if main_component == first_sub_component {
+                // Check all the subcomponents match starting from this index
+                if main_components[index..]
+                    .iter()
+                    .zip(sub_components.iter())
+                    .all(|(main, sub)| main == sub)
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
+
+/// Check ffmpeg is found in PATH.
+pub fn ffmpeg_available() -> bool {
+    Command::new("ffmpeg").arg("-version").output().is_ok()
+}
+
+/// Convert the given path to be relative to the current working directory.
+/// Returns the original path if the relative path cannot be created.
+pub fn get_relative_path_from_current_working_directory(path: &Path) -> PathBuf {
+    env::current_dir()
+        .map(|current_dir| path.strip_prefix(&current_dir).unwrap_or(path).to_path_buf())
+        .unwrap_or(path.to_path_buf())
+}
+
+/// Convert filename to artist and title tags.
+/// Expects filename to be in format 'artist - title'.
+pub fn get_tags_from_filename(filename: &str) -> Option<(String, String)> {
+    if !filename.contains(" - ") {
+        eprintln!(
+            "{}",
+            format!("Can't parse full tag data from malformed filename: {filename}").yellow()
+        );
+        return if filename.is_empty() {
+            None
+        } else {
+            Some((String::new(), filename.to_string()))
+        };
+    }
+
+    let parts: Vec<&str> = filename.splitn(2, " - ").collect();
+    if parts.len() == 2 {
+        let artist = normalize_str(parts[0]);
+        let title = normalize_str(parts[1]);
+        Some((artist, title))
+    } else {
+        None
+    }
+}
+
+pub fn normalize_str(input: &str) -> String {
+    input.nfc().collect::<String>()
+}
+
+/// Convert path to string with invalid unicode handling.
+pub fn path_to_string(path: &Path) -> String {
+    if let Some(string) = path.to_str() {
+        string.to_string()
+    } else {
+        let string = path.to_string_lossy().to_string().replace('\u{FFFD}', "");
+        eprintln!("{}", "Path contains invalid unicode".red());
+        eprintln!("{:?}", path);
+        eprintln!("{}", string);
+        string
+    }
+}
+
+/// Get relative path and convert to string with invalid unicode handling.
+pub fn path_to_string_relative(path: &Path) -> String {
+    path_to_string(&get_relative_path_from_current_working_directory(path))
+}
+
 /// Print a single line diff of the changes.
 pub fn print_diff(old: &str, new: &str) {
     let (old_diff, new_diff) = color_diff(old, new);
@@ -113,6 +165,24 @@ pub fn print_stacked_diff(old: &str, new: &str) {
 /// Print a divider line that matches the length of the reference text.
 pub fn print_divider(text: &str) {
     println!("{}", "-".repeat(text.chars().count()));
+}
+
+/// Try to read tags from file.
+/// Will return empty tags when there are no tags.
+pub fn read_tags(track: &Track) -> Option<Tag> {
+    match Tag::read_from_path(&track.path) {
+        Ok(tag) => Some(tag),
+        Err(Error {
+            kind: ErrorKind::NoTag, ..
+        }) => {
+            println!("\n{}", format!("No tags: {}", track).yellow());
+            Some(Tag::new())
+        }
+        Err(error) => {
+            eprintln!("\n{}", format!("Failed to read tags for: {}\n{}", track, error).red());
+            None
+        }
+    }
 }
 
 /// Rename track from given path to new path.
@@ -149,76 +219,6 @@ pub fn write_genre_log(genres: &[(String, usize)]) -> anyhow::Result<()> {
 
     println!("Logged genres to: {}", dunce::canonicalize(filepath)?.display());
     Ok(())
-}
-
-/// Format bool value as a coloured string
-pub fn colorize_bool(value: bool) -> ColoredString {
-    if value {
-        "true".green()
-    } else {
-        "false".yellow()
-    }
-}
-
-/// Check ffmpeg is found in PATH.
-pub fn ffmpeg_available() -> bool {
-    Command::new("ffmpeg").arg("-version").output().is_ok()
-}
-
-/// Convert filename to artist and title tags.
-/// Expects filename to be in format 'artist - title'.
-pub fn get_tags_from_filename(filename: &str) -> Option<(String, String)> {
-    if !filename.contains(" - ") {
-        eprintln!(
-            "{}",
-            format!("Can't parse full tag data from malformed filename: {filename}").yellow()
-        );
-        return if filename.is_empty() {
-            None
-        } else {
-            Some((String::new(), filename.to_string()))
-        };
-    }
-
-    let parts: Vec<&str> = filename.splitn(2, " - ").collect();
-    if parts.len() == 2 {
-        let artist = normalize_str(parts[0]);
-        let title = normalize_str(parts[1]);
-        Some((artist, title))
-    } else {
-        None
-    }
-}
-
-pub fn normalize_str(input: &str) -> String {
-    input.nfc().collect::<String>()
-}
-
-/// Check if the given path contains the sub path.
-pub fn contains_subpath(main_path: &Path, subpath: &Path) -> bool {
-    let main_components: Vec<_> = main_path.components().collect();
-    let sub_components: Vec<_> = subpath.components().collect();
-
-    if sub_components.len() > main_components.len() {
-        return false;
-    }
-
-    // Find the start index of the first subpath component in the main path
-    if let Some(first_sub_component) = sub_components.first() {
-        for (index, main_component) in main_components.iter().enumerate() {
-            if main_component == first_sub_component {
-                // Check all the subcomponents match starting from this index
-                if main_components[index..]
-                    .iter()
-                    .zip(sub_components.iter())
-                    .all(|(main, sub)| main == sub)
-                {
-                    return true;
-                }
-            }
-        }
-    }
-    false
 }
 
 #[cfg(test)]
